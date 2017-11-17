98b99803c254c0d16b9158719d7a0bb0d5f38443
Juan Patricio Di Bacco <jpdibacco@gmail.com>
2017-11-17 03:16:10 -0300

    added basic openweathermap api

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
-console.log('\'Allo \'Allo!');
-var latitude, longitude, accuracy;
+var latitude, longitude, accuracy, appid = '83d1fe91528b3e86e6d74be49baef889',
+    api_url;
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success_handler,
         error_handler, { enableHighAccuracy: true });
@@ -13,12 +13,29 @@ function success_handler(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
     accuracy = position.coords.accuracy;
-    console.log('posLat:', latitude);
-    console.log("posLon", longitude);
-    console.log("posAccuracy", accuracy);
-    // $.cookie("posLat", latitude);
-    // $.cookie("posLon", longitude);
-    // $.cookie("posAccuracy", accuracy);
+    api_url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=metric&APPID=' + appid;
+    // console.log('posLat: ', latitude);
+    // console.log('posLon: ', longitude);
+    // console.log('posAccuracy: ', accuracy);
+    $.ajax({
+        url: api_url,
+        method: 'GET',
+        statusCode: {
+            404: function() {
+                console.log('Page not found');
+            },
+            401: function() {
+                console.log('Unauthorized')
+            }
+        },
+        success: function(data) {
+            console.log(data.main.temp);
+            var tempr = data.main.temp;
+            var location = data.name;
+            var desc = data.weather.description;
+            console.log(tempr + '°' + location);
+        }
+    });
 }
 
 function error_handler() {
